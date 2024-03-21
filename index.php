0148a8afafc69396b55de5eb4588e88ef7b7f344
Hoki Attila <hoki.attila@gmail.com>
2024-03-21 19:04:27 +0100

    Added view.php to fit the MVC structure

--- index.php
+++ index.php
@@ -2,18 +2,4 @@
     require "model.php";
     $model = new Model;
     $products = $model->getData();
-?>
-<!DOCTYPE>
-<html lang="en">
-<head>
-        <title>Products</title>
-        <meta charset="utf-8">
-</head>
-<body>
-    <h1>Products</h1>
-    <?php foreach ($products as $product):?>
-    <h2><?= htmlspecialchars($product["name"])  /* escaping special chars */ ?></h2>
-    <p><?= htmlspecialchars($product["description"]) ?></p>
-    <?php endforeach; ?>
-</body>
-</html>
+    require "view.php";
---
+++ view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE>
+<html lang="en">
+<head>
+    <title>Products</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<h1>Products</h1>
+<?php foreach ($products as $product):?>
+    <h2><?= htmlspecialchars($product["name"])  /* escaping special chars */ ?></h2>
+    <p><?= htmlspecialchars($product["description"]) ?></p>
+<?php endforeach; ?>
+</body>
+</html>
