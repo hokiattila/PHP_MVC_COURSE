7fc4bf541f1f59ed664235a4ed4ef81a242ff355
Hoki Attila <hoki.attila@gmail.com>
2024-03-21 18:18:20 +0100

    Added generic PDO method to fetch data from MySQL database

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    $dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306"; // the default port is 3306 if we don't specify
+    $pdo = new PDO($dsn, "product_db_user", "secret", [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+
+    $stmt =  $pdo->query("SELECT * FROM product");
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    print_r($products);
