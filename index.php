0411483931ffa4cff167efd60ff6ef1c7dbf85c4
Hoki Attila <hoki.attila@gmail.com>
2024-05-16 22:36:40 +0200

    Added dynamic pathmatching with regex processes

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
     });
 
     $router = new Framework\Router;
-    $router->add("/home/index", ["controller" => "home", "action" => "index"]);
-    $router->add("/products", ["controller"=>"products", "action" => "index"]);
-    $router->add("/", ["controller" => "home", "action" => "index"]);
+    $router->add("/{controller}/{action}");
+   // $router->add("/home/index", ["controller" => "home", "action" => "index"]);
+   // $router->add("/products", ["controller"=>"products", "action" => "index"]);
+    // $router->add("/", ["controller" => "home", "action" => "index"]);
 
    $params = $router->match($path);
     if($params === false) exit("No route matched");
